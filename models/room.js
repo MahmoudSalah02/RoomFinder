bf36d339634b8f41d9a7e60c6adb623d8fa550f5
Mahmoud Salah <Mahmoudsalah@brandeis.edu>
2022-04-23 23:56:35 -0400

    finished index,new,show ejs files

--- models/room.js
+++ models/room.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const RoomSchema = new Schema({
-    number: Nunmber,
+    number: Number,
     price: Number,
     building: String,
     image: String,
     rating: Number,
+    review: String,
 });
 
 module.exports = mongoose.model('Room', RoomSchema);
