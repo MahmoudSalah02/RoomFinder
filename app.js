8a5ba0e53fcc4423e0142d1dff68f9500d040299
Mahmoud Salah <Mahmoudsalah@brandeis.edu>
2022-04-24 00:29:41 -0400

    added layouts, partials, and styled with bootstrap

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Room = require('./models/room');
-
+const ejsMate = require('ejs-mate');
 
 const app = express();
 
-
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
